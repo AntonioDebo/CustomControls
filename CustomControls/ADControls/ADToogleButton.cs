9a2fc501dda9199708d6a3c136a630a359658c61
Antonio Debo <antonio.debo@gmail.com>
2023-08-28 09:49:08 -0600

    Agregar archivos de proyecto.

---
+++ CustomControls/ADControls/ADToogleButton.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace CustomControls.ADControls
+{
+    public class ADToogleButton: CheckBox
+    {
+        //Fields
+        private Color onBackColor = Color.MediumSlateBlue;
+        private Color onToogleColor= Color.WhiteSmoke;
+        private Color offBackColor = Color.Gray;
+        private Color offToogleColor = Color.Gainsboro;
+        private bool solidStyle = true;
+
+        [ DefaultValue(true)]
+        public bool SolidStyle 
+        { 
+            get 
+            { 
+                return (solidStyle == true);
+            }
+            set 
+            { 
+                solidStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color OnBackColor 
+        {
+            get
+            {
+                return onBackColor;
+            }
+            set 
+            { 
+                onBackColor = value;
+                this.Invalidate();
+            }
+        }
+        public Color OnToogleColor 
+        {
+            get
+            {
+                return onToogleColor;
+            }
+            set
+            {
+                onToogleColor = value;
+                this.Invalidate();
+            }
+        }
+        public Color OffBackColor 
+        {
+            get
+            {
+                return offBackColor;
+            }
+            set
+            {
+
+                offBackColor = value;
+                this.Invalidate();
+            }
+
+        }
+        public Color OffToogleColor 
+        {
+            get 
+            { 
+                return offToogleColor; 
+            } 
+            set 
+            { 
+                offToogleColor = value; 
+                this.Invalidate(); 
+            }
+        }
+
+        //Constructor
+        public ADToogleButton()
+        {
+            this.MinimumSize = new Size(45, 22);
+        }
+
+        //Methods
+        private GraphicsPath GetFigurePath()
+        {
+            int arcSize = this.Height - 1;
+            Rectangle leftArc = new Rectangle(0,0,arcSize, arcSize);
+            Rectangle rightArc = new Rectangle(this.Width-arcSize-2, 0, arcSize, arcSize);
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(leftArc, 90, 180);
+            path.AddArc(rightArc, 270, 180);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            int toogleSize = this.Height - 5;
+            pevent.Graphics.SmoothingMode=SmoothingMode.AntiAlias;
+            pevent.Graphics.Clear(this.Parent.BackColor);
+
+            if (this.Checked )// Estado ON
+            {
+                //Draw the control surface
+                if(solidStyle)
+                pevent.Graphics.FillPath(new SolidBrush(onBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(onBackColor,2),GetFigurePath());
+                //Draw the toogle
+                pevent.Graphics.FillEllipse(new SolidBrush(onToogleColor), 
+                    new Rectangle(this.Width-this.Height+1,2,toogleSize,toogleSize));
+            }
+            else// Estado OFF
+            {
+                //Draw the control surface
+                if (solidStyle)
+                pevent.Graphics.FillPath(new SolidBrush(offBackColor), GetFigurePath());
+                else pevent.Graphics.DrawPath(new Pen(offBackColor,2),GetFigurePath());
+                //Draw the toogle
+                pevent.Graphics.FillEllipse(new SolidBrush(offToogleColor),
+                    new Rectangle(2, 2, toogleSize, toogleSize));
+            }
+        }
+        
+    }
+}
---
+++ CustomControls/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CustomControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adToogleButton2 = new CustomControls.ADControls.ADToogleButton();
+            this.adToogleButton1 = new CustomControls.ADControls.ADToogleButton();
+            this.SuspendLayout();
+            // 
+            // adToogleButton2
+            // 
+            this.adToogleButton2.Location = new System.Drawing.Point(40, 130);
+            this.adToogleButton2.MinimumSize = new System.Drawing.Size(45, 22);
+            this.adToogleButton2.Name = "adToogleButton2";
+            this.adToogleButton2.OffBackColor = System.Drawing.Color.Gray;
+            this.adToogleButton2.OffToogleColor = System.Drawing.Color.Gainsboro;
+            this.adToogleButton2.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.adToogleButton2.OnToogleColor = System.Drawing.Color.MediumSlateBlue;
+            this.adToogleButton2.Size = new System.Drawing.Size(64, 39);
+            this.adToogleButton2.SolidStyle = false;
+            this.adToogleButton2.TabIndex = 1;
+            this.adToogleButton2.Text = "adToogleButton2";
+            this.adToogleButton2.UseVisualStyleBackColor = true;
+            // 
+            // adToogleButton1
+            // 
+            this.adToogleButton1.AutoSize = true;
+            this.adToogleButton1.Location = new System.Drawing.Point(40, 48);
+            this.adToogleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.adToogleButton1.Name = "adToogleButton1";
+            this.adToogleButton1.OffBackColor = System.Drawing.Color.Gray;
+            this.adToogleButton1.OffToogleColor = System.Drawing.Color.Gainsboro;
+            this.adToogleButton1.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.adToogleButton1.OnToogleColor = System.Drawing.Color.WhiteSmoke;
+            this.adToogleButton1.Size = new System.Drawing.Size(133, 22);
+            this.adToogleButton1.TabIndex = 0;
+            this.adToogleButton1.Text = "adToogleButton1";
+            this.adToogleButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.adToogleButton2);
+            this.Controls.Add(this.adToogleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ADControls.ADToogleButton adToogleButton1;
+        private ADControls.ADToogleButton adToogleButton2;
+    }
+}
+
