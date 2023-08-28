5c9e021cf3747021e3fc62e1a46af567a05e9931
Antonio Debo <antonio.debo@gmail.com>
2023-08-28 10:59:38 -0600

    Custom button added

--- CustomControls/Form1.Designer.cs
+++ CustomControls/Form1.Designer.cs
@@ -28,9 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.adButton2 = new CustomControls.ADControls.ADButton();
+            this.adButton1 = new CustomControls.ADControls.ADButton();
             this.adToogleButton2 = new CustomControls.ADControls.ADToogleButton();
             this.adToogleButton1 = new CustomControls.ADControls.ADToogleButton();
+            this.adButton3 = new CustomControls.ADControls.ADButton();
             this.SuspendLayout();
+            // 
+            // adButton2
+            // 
+            this.adButton2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.adButton2.BorderRadius = 10;
+            this.adButton2.BorderSize = 0;
+            this.adButton2.FlatAppearance.BorderSize = 0;
+            this.adButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adButton2.ForeColor = System.Drawing.Color.White;
+            this.adButton2.Location = new System.Drawing.Point(341, 223);
+            this.adButton2.Name = "adButton2";
+            this.adButton2.Size = new System.Drawing.Size(150, 64);
+            this.adButton2.TabIndex = 3;
+            this.adButton2.Text = "TextButton";
+            this.adButton2.TextColor = System.Drawing.Color.White;
+            this.adButton2.UseVisualStyleBackColor = false;
+            // 
+            // adButton1
+            // 
+            this.adButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.adButton1.BorderRadius = 10;
+            this.adButton1.BorderSize = 0;
+            this.adButton1.FlatAppearance.BorderSize = 0;
+            this.adButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adButton1.ForeColor = System.Drawing.Color.White;
+            this.adButton1.Location = new System.Drawing.Point(345, 130);
+            this.adButton1.Name = "adButton1";
+            this.adButton1.Size = new System.Drawing.Size(150, 59);
+            this.adButton1.TabIndex = 2;
+            this.adButton1.Text = "adButton1";
+            this.adButton1.TextColor = System.Drawing.Color.White;
+            this.adButton1.UseVisualStyleBackColor = false;
             // 
             // adToogleButton2
             // 
@@ -62,11 +101,32 @@
             this.adToogleButton1.Text = "adToogleButton1";
             this.adToogleButton1.UseVisualStyleBackColor = true;
             // 
+            // adButton3
+            // 
+            this.adButton3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton3.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.adButton3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.adButton3.BorderRadius = 10;
+            this.adButton3.BorderSize = 0;
+            this.adButton3.FlatAppearance.BorderSize = 0;
+            this.adButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adButton3.ForeColor = System.Drawing.Color.White;
+            this.adButton3.Location = new System.Drawing.Point(89, 263);
+            this.adButton3.Name = "adButton3";
+            this.adButton3.Size = new System.Drawing.Size(150, 53);
+            this.adButton3.TabIndex = 4;
+            this.adButton3.Text = "adButton3";
+            this.adButton3.TextColor = System.Drawing.Color.White;
+            this.adButton3.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.adButton3);
+            this.Controls.Add(this.adButton2);
+            this.Controls.Add(this.adButton1);
             this.Controls.Add(this.adToogleButton2);
             this.Controls.Add(this.adToogleButton1);
             this.Name = "Form1";
@@ -80,6 +140,9 @@
 
         private ADControls.ADToogleButton adToogleButton1;
         private ADControls.ADToogleButton adToogleButton2;
+        private ADControls.ADButton adButton1;
+        private ADControls.ADButton adButton2;
+        private ADControls.ADButton adButton3;
     }
 }
 
